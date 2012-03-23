3c671206250408101cb9728b94a889bf7cc83f84
gite0002 <zohana28@yahoo.com>
2012-03-23 15:24:23 -0400

    rate.php:made a new page for rating the system single.php:chanded stuff and updated.

--- single.php
+++ single.php
@@ -1,53 +1,76 @@
-<?php 
+<?php
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if (empty($id)) {
+	header('Location: index.php');
+	exit;
+}
 
 require_once 'includes/db.php';
+require_once 'includes/functions.php';
 
-$id=filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
-
-if(empty($id)){
-	header('Location:index.php');
-	exit; //stop php compiler right here and immediately redirect the user.
-}
-//only connect to the data base if there is an id, because this is after the above if-statement.
-//without an ID there is no point connecting to the data base.
-
-// -->prepare() allows us to execute SQL with user input.
-$sql = $db->prepare('SELECT id, name, longitude, latitude
-					 FROM museums
-					 WHERE id= :id
- 
+$sql = $db->prepare('
+	SELECT id, name, adr,  longitude, latitude, rate_count, rate_total
+	FROM museums
+	WHERE id = :id
 ');
 
-//->bindValue()lets us fill in the placeholders in our prepared statement
-//:id is a placeholder for us to SECURELY put information from the user
-$sql->bindValue(':id',$id, PDO::PARAM_INT);
-//performs the sql on the database
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
 $sql->execute();
-//gets the results from the sql query and stores them in a variable
-//-->fetch()gets a single result
-//->fetchAll() gets all possible results
-$results=$sql->fetch();
+$museums = $sql->fetch();
 
-//Redirect the user back to the homepage if there are no data base results
-//no results will happen if they change the ?id=4 to include anID that doesnt exist
-if(empty($results)){
-	header('Location:index.php');
-	exit; //stop php compiler right here and immediately redirect the user.
+if (empty($museums)) {
+	header('Location: index.php');
+	exit;
 }
+
+$title = $museums['name'];
+
+if ($museums['rate_count'] > 0) {
+	$rating = round($museums['rate_total'] / $museums['rate_count']);
+} else {
+	$rating = 0;
+}
+
+$cookie = get_rate_cookie();
+
+include 'includes/theme-top.php';
+
 ?>
 
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Ottawa Museum</title>
-</head>
+<h1><?php echo $museums['name']; ?></h1>
 
-<body>
-   <h1><?php echo $results['name'];?></h1>
-   <p>longitude: <?php echo $results['longitude'];?></p>
-   <p>latitude: <?php echo $results['latitude'];?></p>
+<dl>
+	<dt>Average Rating</dt><dd><meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter></dd>
+	<dt>Address</dt><dd><?php echo $museums['adr']; ?></dd>
+	<dt>Longitude</dt><dd><?php echo $museums['longitude']; ?></dd>
+	<dt>Latitude</dt><dd><?php echo $museums['latitude']; ?></dd>
+</dl>
 
+<?php if (isset($cookie[$id])) : ?>
 
-</body>
-</html>
+<h2>Your rating</h2>
+<ol class="rater rater-usable">
+	<?php for ($i = 1; $i <= 5; $i++) : ?>
+		<?php $class = ($i <= $cookie[$id]) ? 'is-rated' : ''; ?>
+		<li class="rater-level <?php echo $class; ?>">★</li>
+	<?php endfor; ?>
+</ol>
+
+<?php else : ?>
+
+<h2>Rate</h2>
+<ol class="rater rater-usable">
+	<?php for ($i = 1; $i <= 5; $i++) : ?>
+	<li class="rater-level"><a href="rate.php?id=<?php echo $museums['id']; ?>&rate=<?php echo $i; ?>">★</a></li>
+	<?php endfor; ?>
+</ol>
+
+<?php endif; ?>
+
+<?php
+
+include 'includes/theme-bottom.php';
+
+?>
