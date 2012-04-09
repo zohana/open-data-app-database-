255ad50cf08adb968434f0d37d152683797d5239
gite0002 <zohana28@yahoo.com>
2012-04-09 14:19:04 -0400

    push

--- single.php
+++ single.php
@@ -42,9 +42,9 @@ include 'includes/theme-top.php';
 <h2><?php echo $museums['name']; ?></h2>
 <div class="ratedeco">
 <dl class="stuff">
-	<dt>Average Rating</dt><dd><meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter></dd>
-	<dt>Address</dt><dd><?php echo $museums['adr']; ?></dd>
-	<dt>Longitude</dt><dd><?php echo $museums['longitude']; ?></dd>
+	<dt><strong>Average Rating:</strong></dt><dd><meter value="<?php echo $rating; ?>" min="0" max="5"><?php echo $rating; ?> out of 5</meter></dd>
+	<dt><strong>Address:</strong></dt><dd><?php echo $museums['adr']; ?></dd>
+	<dt><strong>Longitude:</strong></dt><dd><?php echo $museums['longitude']; ?></dd>
 	<dt>Latitude</dt><dd><?php echo $museums['latitude']; ?></dd>
 </dl>
 
