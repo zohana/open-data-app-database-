a4c12770a172cfbdb63a3ad9fb2c2908e798ceb9
gite0002 <zohana28@yahoo.com>
2012-04-13 14:40:23 -0400

    push!

--- admin/index.php
+++ admin/index.php
@@ -29,9 +29,9 @@ include '../includes/theme-top.php';
 		<a href ="../single.php?id=<?php echo $museums['id'];?>"><?php echo $museums['name'] ; ?></a> 
         &bull;
         <div class="edit-stuff">
-           <li> <a class="over1" href ="delete.php?id=<?php echo $museums['id'];?>">Delete</a>
-            <a class="over1" href ="edit.php?id=<?php echo $museums['id'];?>">Edit</a>
-            <a class="over1" href ="add.php?id=<?php echo $museums['id'];?>">Add</a>
+           <li> <a id="over1" href ="delete.php?id=<?php echo $museums['id'];?>">Delete</a>
+            <a id="over1" href ="edit.php?id=<?php echo $museums['id'];?>">Edit</a>
+            <a id="over1" href ="add.php?id=<?php echo $museums['id'];?>">Add</a>
             </li>
        	</div>
         
